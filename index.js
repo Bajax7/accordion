b04e1b2677b5b611789c61cdd8f2a4bc00a0da74
Bajax7 <bajax07@gmail.com>
2023-07-06 02:30:15 +0800

    first javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+function toggle1(){
+    let p1 = document.getElementById('1');
+    p1.classList.toggle('none');
+}
+function toggle2(){
+    let p2 = document.getElementById('2');
+    p2.classList.toggle('none');
+}
+function toggle3(){
+    let p3 = document.getElementById('3');
+    p3.classList.toggle('none');
+}
+function toggle4(){
+    let p4 = document.getElementById('4');
+    p4.classList.toggle('none');
+}
+function toggle5(){
+    let p5 = document.getElementById('5');
+    p5.classList.toggle('none');
+}
